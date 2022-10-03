efda2c5037da1c123adbc8118816c3ac63f37d7f
Sam <pn88doo@eduvaud.ch>
2022-10-03 14:19:03 +0200

    Update Aoc_3.php

--- Codes/3/Aoc_3.php
+++ Codes/3/Aoc_3.php
@@ -1,4 +1,5 @@
 <?php 
+// Take the input and returns an array
 function getData($path)
 {
     $lines = [];
@@ -16,12 +17,69 @@ function getData($path)
 
 $input = getData("input.txt");
 
-$gammaRate = array(12);
-$epsilonRate = array(12);
-$onesCounter;
-$zerosCounter;
+// array of bits for the gammaRate
+$gammaRate = array(12);  
+// array of bits for the epsilonRate
+$epsilonRate = array(12);  
 
-for($i = 0;$i < count($input);$i++){
 
+
+// -------------  GammaRate ----------- //
+
+// loop that indicate the position of the bit we're counting
+for($x = 0;$x < 12;$x++){
+    // counter for the ones
+    $onesCounter = 0;
+    // counter for the zeros
+    $zerosCounter = 0;
+    
+    // for loop that counts how many times 1 is in the start of each lines
+    for($i = 0;$i < count($input);$i++){
+        if(substr($input[$i],$x,1) == '1'){
+            $onesCounter++;
+        }
+        else{
+            $zerosCounter++;
+        }
+    }
+    
+    if($onesCounter > $zerosCounter){
+        $gammaRate[$x] = 1;
+    }
+    else{
+        $gammaRate[$x] = 0;
+    }
 }
+
+// ------------ EpsilonRate ---------- // 
+
+// counter for the ones (setting it to 0)
+$onesCounter = 0;
+// counter for the zeros (setting it to 0)
+$zerosCounter = 0; 
+
+
+for($x = 0;$x < 12;$x++){
+    // for loop that counts how many times 0 is in the start of each lines
+    for($i = 0;$i < count($input);$i++){
+        if(substr($input[$i],$x,1) == '0'){
+            $onesCounter++;
+        }
+        else{
+            $zerosCounter++;
+        }
+    }
+    
+    if($onesCounter > $zerosCounter){
+        $epsilonRate[$x] = 1;
+    }
+    else{
+        $epsilonRate[$x] = 0;
+    }
+}
+
+$gammaRateBinary = implode("",$gammaRate);
+$epsilonRateBinary = implode("",$epsilonRate);
+var_dump($input);
+var_dump($gammaRateBinary);
 ?>
