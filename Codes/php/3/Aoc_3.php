a0d89b5a7285e2b924585843482c12e0eac35375
Sam <pn88doo@eduvaud.ch>
2022-10-10 14:06:07 +0200

    Update Aoc_3.php

--- Codes/php/3/Aoc_3.php
+++ Codes/php/3/Aoc_3.php
@@ -84,67 +84,11 @@ echo("first part" . bindec($gammaRateBinary)+bindec($epsilonRateBinary));
 
 // ------------------- SECOND PART --------------------------- // 
 
+// counter for the ones (setting it to 0)
+$onesCounter = 0;
+// counter for the zeros (setting it to 0)
+$zerosCounter = 0; 
 
-for($h = 0;$h < 12;$h++){
-
-    // counter for the ones
-    $onesCounter = 0;
-    // counter for the zeros
-    $zerosCounter = 0;
-
-    // Counter loop
-    for($i = 0;$i < count($input);$i++){
-        if(substr($input[$h],$h,1) == '1'){
-           $onesCounter++;
-        }
-        else{
-           $zerosCounter++;
-        }
-    }
-    
-    // if there's more ones than zeros replace all zeros by 2 (so i dont use them in the counter loop)
-    if($onesCounter > $zerosCounter){
-        for($i = 0;$i < count($input);$i++){
-            if(substr($input[$i],$h,1) == '0'){
-               $input[$i] = 2;
-            }
-        }
-    }
-    // if there's an equal amount we take the zeros by default
-    elseif($onesCounter == $zerosCounter){
-        for($i = 0;$i < count($input);$i++){
-            if(substr($input[$i],$h,1) == '0'){
-               $input[$i] = 2;
-            }
-        }
-    }
-    // if there's more zeros than ones replace all ones by 2
-    else{
-        for($i = 0;$i < count($input);$i++){
-            if(substr($input[$i],$h,1) == '1'){
-               $input[$i] = 2;
-            }
-        }
-    }
-
-    // if there are equal numbers
-    if($zerosCounter == 0 && $onesCounter == 1){
-        for($i = 0;$i < count($input);$i++){
-            if(substr($input[$h],$h,1) == '1'){
-              $endOne = $input[$i];
-            }       
-        }
-        break;
-    }
-    elseif($zerosCounter == 1 && $onesCounter == 0){
-        for($i = 0;$i < count($input);$i++){
-            if(substr($input[$i],$h,1) == '0'){
-              $endTwo = $input[$i];
-            }       
-        }
-        break; 
-    }
-}
 echo(bindec($endOne) . "  " . bindec($endTwo));
 
 
