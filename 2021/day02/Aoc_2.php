b40359581c7681d8210c2dc20f928dcd3a7f1dcc
Sam <pn88doo@eduvaud.ch>
2022-10-11 13:11:44 +0200

    Updating files

---
+++ 2021/day02/Aoc_2.php
@@ -0,0 +1,52 @@
+<?php
+
+/// Function who's purpose is to store all data into a defined array
+function getData($path)
+{
+    $lines = [];
+    if ($file = fopen($path, "r")) {
+        while (!feof($file)) {
+            $textperline = fgets($file);
+            $textperline = preg_replace('/[\r\n]+/', '', $textperline);
+            $lines[] = $textperline;
+        }
+        fclose($file);
+    }
+    return $lines;
+}
+
+
+$lines = getData("input02.txt");
+
+
+$horizontalValue = 0;
+$depthValue = 0;
+$aimValue = 0;
+$forward = "forward";
+$up = "up";
+$down = "down";
+
+for($x = 0;$x < count($lines); $x++)
+{
+    if(preg_match("/{$forward}/i", $lines[$x])) {
+        $value = substr($lines[$x],-1); // Take last char (number);
+        $horizontalValue += $value; // if forward we add
+        $depthValue += ($aimValue * substr($lines[$x],-1));   
+             
+    }
+    
+    elseif(preg_match("/{$up}/i", $lines[$x])) {
+        $value = substr($lines[$x],-1);
+        $aimValue -= $value;
+        
+    }
+    elseif(preg_match("/{$down}/i", $lines[$x])) {
+        $value = substr($lines[$x],-1);
+        $aimValue += $value;
+        
+    }
+    
+    
+}
+echo $horizontalValue * $depthValue;
+?>
