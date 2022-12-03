d053a9322185adc387cd1824d2b5280c114dc17d
Sam <pn88doo@eduvaud.ch>
2022-12-03 09:51:24 +0100

    Updates

--- 2022/day03/aoc_day3/aoc_day3/Program.cs
+++ 2022/day03/aoc_day3/aoc_day3/Program.cs
@@ -10,7 +10,35 @@ namespace aoc_day3
     {
         static void Main(string[] args)
         {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
 
+            int[] values = new int[52];
+            int x = 1;
+            string firstHalf = "";
+            string secondHalf = "";
+
+            // priorities values
+            for (int i = 0; i < 52;i++)
+            {
+                values[i] = x;
+                x++;
+            }
+
+            for (int i = 0; i < lines.Length;i++)
+            {
+                firstHalf = lines[i].Substring(0, lines[i].Length/2);
+                secondHalf = lines[i].Substring(lines[i].Length / 2, lines[i].Length / 2);
+                for (int j = 0; j < firstHalf.Length;j++)
+                {
+                    for (int k = 0; k < secondHalf.Length;k++)
+                    {
+                        if (firstHalf[k] == secondHalf)
+                        {
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
