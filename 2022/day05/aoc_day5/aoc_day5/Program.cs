5b304e68d4125ddd7f65ebbcf7ce3962a7f4dd22
Sam <pn88doo@eduvaud.ch>
2022-12-05 10:22:11 +0100

    Starting day 5

---
+++ 2022/day05/aoc_day5/aoc_day5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+
+
+            foreach (string line in System.IO.File.ReadAllLines("input.txt"))
+            {
+                lines.Add(line);
+                Console.WriteLine(line);
+            }
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
