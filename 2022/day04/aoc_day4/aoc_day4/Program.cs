905275ec04b22b084fcd65fcd64b48c014ef4efc
Sam <pn88doo@eduvaud.ch>
2022-12-05 09:21:37 +0100

    Day 4 complete

--- 2022/day04/aoc_day4/aoc_day4/Program.cs
+++ 2022/day04/aoc_day4/aoc_day4/Program.cs
@@ -11,6 +11,35 @@ namespace aoc_day4
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines("input.txt");
+            string[] line;
+            string[] firstDeer;
+            string[] secondDeer;
+            int total = 0;
+            for(int i = 0; i < lines.Length; i++)
+            {
+                line = lines[i].Split(',');
+                firstDeer = line[0].Split('-');
+                secondDeer = line[1].Split('-');
+                if (Convert.ToInt32(firstDeer[0]) <= Convert.ToInt32(secondDeer[0]) && Convert.ToInt32(firstDeer[1]) >= Convert.ToInt32(secondDeer[1]))
+                {
+                    total++;
+                }
+                else if (Convert.ToInt32(secondDeer[1]) >= Convert.ToInt32(firstDeer[0]) && Convert.ToInt32(secondDeer[1]) <= Convert.ToInt32(firstDeer[1]))
+                {
+                    total++;
+                }
+                else if (Convert.ToInt32(secondDeer[0]) >= Convert.ToInt32(firstDeer[0]) && Convert.ToInt32(secondDeer[0]) <= Convert.ToInt32(firstDeer[1]))
+                {
+                    total++;
+                }
+                else if (Convert.ToInt32(secondDeer[0]) <= Convert.ToInt32(firstDeer[0]) && Convert.ToInt32(secondDeer[1]) >= Convert.ToInt32(firstDeer[1]))
+                {
+                    total++;
+                }
+                
+            }
+
+            Console.WriteLine(total);
             Console.ReadLine();
         }
     }
